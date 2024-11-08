8f73c1d28fda639593b7c6fc2e7ba23d6c94d4d0
LeonardodCastro <leonardo.castro@maithatechcorp.onmicrosoft.com>
2024-11-08 18:47:53 -0300

    feat: creating User entity

---
+++ src/main/kotlin/personal/projects/investment_consolidator/entity/User.kt
@@ -0,0 +1,32 @@
+package personal.projects.investment_consolidator.entity
+
+import jakarta.persistence.*
+import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.UpdateTimestamp
+import java.time.Instant
+import java.util.*
+
+@Entity
+@Table(name = "tb_users")
+class User (
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "user_id")
+    val userId: UUID,
+
+    @Column(name = "username")
+    val username: String,
+
+    @Column(name = "email")
+    val email: String,
+
+    @Column(name = "password")
+    val password: String,
+
+    @CreationTimestamp
+    val creationTimestamp: Instant,
+
+    @UpdateTimestamp
+    val updatedTimestamp: Instant
+)
