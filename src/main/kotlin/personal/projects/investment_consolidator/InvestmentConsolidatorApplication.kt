871236ff4bebcd7545d876985297261c44c3c248
LeonardodCastro <leonardo.castro@maithatechcorp.onmicrosoft.com>
2024-11-08 18:33:11 -0300

    Initial commit

---
+++ src/main/kotlin/personal/projects/investment_consolidator/InvestmentConsolidatorApplication.kt
@@ -0,0 +1,11 @@
+package personal.projects.investment_consolidator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InvestmentConsolidatorApplication
+
+fun main(args: Array<String>) {
+	runApplication<InvestmentConsolidatorApplication>(*args)
+}
---
+++ src/test/kotlin/personal/projects/investment_consolidator/InvestmentConsolidatorApplicationTests.kt
@@ -0,0 +1,13 @@
+package personal.projects.investment_consolidator
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InvestmentConsolidatorApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
