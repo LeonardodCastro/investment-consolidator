cb36bbe1bcf605827b0d4d23cbe81662c97af596
LeonardodCastro <leonardo.castro@maithatechcorp.onmicrosoft.com>
2024-11-18 22:00:44 -0300

    test: get all users and nested classes to improve read

--- src/test/kotlin/personal/projects/investment_consolidator/services/UserServiceTest.kt
+++ src/test/kotlin/personal/projects/investment_consolidator/services/UserServiceTest.kt
@@ -8,10 +8,10 @@ import io.mockk.slot
 import io.mockk.verify
 import jakarta.persistence.EntityNotFoundException
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.extension.ExtendWith
-import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException
 import personal.projects.investment_consolidator.controllers.request.CreateUserRequest
 import personal.projects.investment_consolidator.entities.User
 import personal.projects.investment_consolidator.mappers.toEntity
@@ -54,36 +54,54 @@ class UserServiceTest {
         verify(exactly = 1) { userRepository.save(userCaptured) }
     }
 
-    @Test
-    fun `should find an user by given id successfully`() {
-        val idSlot = slot<UUID>()
-        val expectedUser = buildUserEntity()
-        every { userRepository.getReferenceById(capture(idSlot)) } returns expectedUser
+    @Nested
+    inner class GetUserById {
+        @Test
+        fun `should find an user by given id successfully`() {
+            val idSlot = slot<UUID>()
+            val expectedUser = buildUserEntity()
+            every { userRepository.getReferenceById(capture(idSlot)) } returns expectedUser
 
-        val inputId = UUID.randomUUID()
-        val userFound = userService.getUserById(inputId)
+            val inputId = UUID.randomUUID()
+            val userFound = userService.getUserById(inputId)
 
-        val idCaptured = idSlot.captured
+            val idCaptured = idSlot.captured
 
-        assertNotNull(userFound)
-        assertEquals(inputId, idCaptured)
-        assertEquals(userFound.email, expectedUser.email)
-        assertEquals(userFound.password, expectedUser.password)
-        assertEquals(userFound.username, expectedUser.username)
+            assertNotNull(userFound)
+            assertEquals(inputId, idCaptured)
+            assertEquals(userFound.email, expectedUser.email)
+            assertEquals(userFound.password, expectedUser.password)
+            assertEquals(userFound.username, expectedUser.username)
 
-        verify(exactly = 1) { userRepository.getReferenceById(idCaptured) }
-    }
-
-    @Test
-    fun `should throw exception when repository fails`() {
-        val userId = UUID.randomUUID()
-        every { userRepository.getReferenceById(userId) } throws EntityNotFoundException()
-
-        assertThrows<EntityNotFoundException> {
-            userService.getUserById(userId)
+            verify(exactly = 1) { userRepository.getReferenceById(idCaptured) }
         }
 
-        verify(exactly = 1) { userRepository.getReferenceById(userId) }
+        @Test
+        fun `should throw exception when getReferenceById not found`() {
+            val userId = UUID.randomUUID()
+            every { userRepository.getReferenceById(userId) } throws EntityNotFoundException()
+
+            assertThrows<EntityNotFoundException> {
+                userService.getUserById(userId)
+            }
+
+            verify(exactly = 1) { userRepository.getReferenceById(userId) }
+        }
+    }
+
+
+    @Nested
+    inner class GetAllUsers {
+        @Test
+        fun `should get all users successfully`() {
+
+            val expectedUsers = listOf(buildUserEntity(), buildUserEntity())
+            every { userRepository.findAll() } returns expectedUsers
+
+            val usersFound = userService.getAllUsers()
+
+            assertEquals(expectedUsers[0].username, usersFound!![0].username)
+        }
 
     }
 
