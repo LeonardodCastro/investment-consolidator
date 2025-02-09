084b43af48322dd253ddd3641a6749f78edd6564
LeonardodCastro <leonardo.castro@maithatechcorp.onmicrosoft.com>
2025-02-09 20:27:36 -0300

    fix: renaming token variable

---
+++ src/test/kotlin/personal/projects/investment_consolidator/services/AccountServiceTest.java
@@ -0,0 +1,12 @@
+package personal.projects.investment_consolidator.services;
+
+import com.github.tomakehurst.wiremock.junit5.WireMockTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@WireMockTest(httpPort = 8081)
+public class AccountServiceTest {
+
+
+
+}
